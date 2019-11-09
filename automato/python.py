555cfd245cde2a6b02d002121c27616a3badebb7
Jesulino Felipe <jfelipemm10@gmail.com>
2019-11-08 22:23:40 -0300

    Primeira versao do automato

---
+++ automato/python.py
@@ -0,0 +1,48 @@
+def ehInteiro(texto):
+    try: 
+        int(texto)
+        return True
+    except ValueError:
+        return False
+
+def getTrecho(texto):
+    trecho = ""
+    if str.isalpha(texto[0]):
+        i = 0
+        while (str.isalpha(c[i])):
+            trecho += c[i]
+            i += 1
+        return trecho
+    else:
+        i = 0
+        while (ehInteiro(c[i])):
+            trecho += c[i]
+            i += 1
+        return trecho
+
+def retornaToken(texto):
+    if (str.isalpha(texto[0])):
+        return "<identificador, " + texto + ">"
+    elif (ehInteiro(texto[0])):
+        return "<inteiro, " + texto + ">"
+    elif (texto == "+"):
+        return "<soma,>"
+    elif (texto == "-"):
+        return "<subtração,>"
+    elif (texto == "*"):
+        return "<multiplicação,>"
+    elif (texto == "/"):
+        return "<divisão,>"
+    elif (texto == "="):
+        return "<igualdade,>"
+    else:
+        return "<undefined>"
+        
+
+name = input("Insira a expressão: ")
+while (name != ""):
+    for palavra in name.split():
+        token = retornaToken(palavra)
+        print(token, end = '')
+    print()
+    name = input("Insira a expressão: ")
